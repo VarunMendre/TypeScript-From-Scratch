c625a7cb5910f2694bb68dadc307b9d9da6c301e
Varun Mendre <varunmm0404@gmail.com>
2026-06-13 15:11:39 +0530

    first commit

---
+++ 01_what-is-ts-and-working/app.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+// let obj = {
+//     name: "Varun"
+// };
+
+
+// let name = "Varun";
+
+// name.toUpperCase();
+// Object.assign()
+
+let arr = [1, 2, 3]
+
+console.log(arr.);
+
+
+// C:\Users\admin\AppData\Local\Programs\Microsoft VS Code\Code.exe 
---
+++ 01_what-is-ts-and-working/server.js
@@ -0,0 +1,3 @@
+let name = "Varun";
+
+name = 55;
