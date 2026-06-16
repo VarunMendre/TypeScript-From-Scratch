96b7e4c0b41a562df388d36bfc7e5a18bb070c3c
Varun Mendre <varunmm0404@gmail.com>
2026-06-16 23:15:37 +0530

    feat & impl: static/dynamic & strongly/weakly typed languages

---
+++ Section_2_TypeScript-Fundamentals-and-Compiler-config/07_Statically-vs-Dynamically-Typed-Languages/app.js
@@ -0,0 +1,10 @@
+// let a = 5;
+// a.toUpperCase();
+
+// const user = {
+//     name: "Varun"
+// };
+
+// console.log(user.age);
+
+// export {}
---
+++ Section_2_TypeScript-Fundamentals-and-Compiler-config/07_Statically-vs-Dynamically-Typed-Languages/app.ts
@@ -0,0 +1,9 @@
+// let a = 5;
+// a.toUpperCase();
+
+const user = {
+  name: "Varun",
+  age: 22,
+};
+
+// console.log(user.age());
